e62f20574aeff633fcb1cbbf25f9a317636aab73
Assekour-Mohamed <72951139+Assekour-Mohamed@users.noreply.github.com>
2023-10-08 14:26:24 +0100

    Create MonthsDays.py

---
+++ MonthsDays.py
@@ -0,0 +1,28 @@
+m = int(input("Enter the month (1-12): "))
+
+if m == 1:
+    print("31 days")
+elif m == 2:
+    print("28 or 29 days")
+elif m == 3:
+    print("31 days")
+elif m == 4:
+    print("30 days")
+elif m == 5:
+    print("31 days")
+elif m == 6:
+    print("30 days")
+elif m == 7:
+    print("31 days")
+elif m == 8:
+    print("31 days")
+elif m == 9:
+    print("30 days")
+elif m == 10:
+    print("31 days")
+elif m == 11:
+    print("30 days")
+elif m == 12:
+    print("31 days")
+else:
+    print("Invalid month. Please enter a number between 1 and 12.")
